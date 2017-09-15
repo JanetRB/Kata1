8f269c7d71f43d7ef619d1d7f4426014c9248318
usuario <usuario@10.22.148.61>
2017-09-15 09:48:41 +0100

    Kata1 Version2 : Con la clase Calendar

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,14 +1,17 @@
 
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class Kata1 {
 
     
     public static void main(String[] args) {
-        Person person = new Person("Janet", new Date(88,10,24));
-        System.out.println(person.getName() + " tiene " + person.getAge() + " años");
+        Calendar date = GregorianCalendar.getInstance();
+        date.set(1988,10,24);
+        Person person = new Person("Janet", date);
+        System.out.println(person.getName() + " tiene " + person.getAge() + " años" );
     }
 }
